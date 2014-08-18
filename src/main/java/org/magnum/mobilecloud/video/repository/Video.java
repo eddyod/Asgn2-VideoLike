1054dbc73abb045102035c90aa89c1e4f0acbd5d
eodonnell <eodonnell@som2vc6rw1.ad.ucsd.edu>
2014-08-18 15:11:39 -0700

    Added entity to Video class.

--- src/main/java/org/magnum/mobilecloud/video/repository/Video.java
+++ src/main/java/org/magnum/mobilecloud/video/repository/Video.java
@@ -1,30 +1,36 @@
 package org.magnum.mobilecloud.video.repository;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.google.common.base.Objects;
 
 /**
  * A simple object to represent a video and its URL for viewing.
  * 
- * You probably need to, at a minimum, add some annotations to this
- * class.
+ * You probably need to, at a minimum, add some annotations to this class.
  * 
- * You are free to add annotations, members, and methods to this
- * class. However, you probably should not change the existing
- * methods or member variables. If you do change them, you need
- * to make sure that they are serialized into JSON in a way that
- * matches what is expected by the auto-grader.
+ * You are free to add annotations, members, and methods to this class. However,
+ * you probably should not change the existing methods or member variables. If
+ * you do change them, you need to make sure that they are serialized into JSON
+ * in a way that matches what is expected by the auto-grader.
  * 
  * @author mitchell
  */
+@Entity
 public class Video {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
 	private String name;
 	private String url;
 	private long duration;
 	private long likes;
-	
+
 	public Video() {
 	}
 
@@ -71,11 +77,11 @@ public class Video {
 	public long getLikes() {
 		return likes;
 	}
-	
+
 	public void setLikes(long likes) {
 		this.likes = likes;
 	}
-	
+
 	/**
 	 * Two Videos will generate the same hashcode if they have exactly the same
 	 * values for their name, url, and duration.
@@ -97,9 +103,7 @@ public class Video {
 		if (obj instanceof Video) {
 			Video other = (Video) obj;
 			// Google Guava provides great utilities for equals too!
-			return Objects.equal(name, other.name)
-					&& Objects.equal(url, other.url)
-					&& duration == other.duration;
+			return Objects.equal(name, other.name) && Objects.equal(url, other.url) && duration == other.duration;
 		} else {
 			return false;
 		}
